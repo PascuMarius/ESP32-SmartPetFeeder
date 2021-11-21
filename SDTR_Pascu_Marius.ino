b700992e562a6f59eb8b3d332f42fbff42465fa8
PascuMarius <72869172+PascuMarius@users.noreply.github.com>
2021-11-21 11:49:47 +0200

    Added 3 tasks

--- SDTR_Pascu_Marius.ino
+++ SDTR_Pascu_Marius.ino
@@ -1,8 +1,8 @@
-#if CONFIG_FREERTOS_UNICORE
-#define ARDUINO_RUNNING_CORE 0
-#else
-#define ARDUINO_RUNNING_CORE 1
-#endif
+////Pins Definition////
+#define relayPin 1
+#define waterSensorPin 1
+#define scalePin0 16     // 16 - yellow wire
+#define scalePin1 17     // 17 - orange wire 
 
 //// Real time clock library
 #include <TimeLib.h>
@@ -17,11 +17,11 @@ const String urlGet = "/marius.index/_search/";
 const String urlDelete = "/marius.index/_delete_by_query/";
 const char *host = "8f9677360fc34e2eb943d737b2597c7b.us-east-1.aws.found.io";
 const int httpsPort = 9243;
-const String userpass = "******"; // format user:pass
+const String userpass = "CONFIDENTIAL"; // format user:pass
 
 ////Variables for cloud////
-String coltrolFood_e = "";
-String coltrolWater_e = "";
+String controlFood_e = "done";
+String controlWater_e = "";
 int food_weight = 0;
 int waterLevel = 0;
 int selectedFood = 0;
@@ -41,6 +41,7 @@ void TaskAnalogReadA3( void *pvParameters );
 const char* ssid = "Net slab 2g";
 const char* password = "prahova6net";
 
+
 void setup() {
   WiFi.mode(WIFI_STA);
   Serial.begin(115200);
@@ -48,7 +49,7 @@ void setup() {
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    delay(1000);
+    vTaskDelay(1000);
   }
   Serial.println("CONNECTED!");
 
@@ -58,7 +59,7 @@ void setup() {
   while (time(nullptr) <= 100000) 
   {
     Serial.print("*");
-    delay(100);
+    vTaskDelay(100);
   }
   time_t now = time(nullptr);
   setTime(now);
@@ -66,34 +67,44 @@ void setup() {
   Serial.println(); Serial.print("IP address: "); Serial.println(WiFi.localIP());
   Serial.print("TimeStamp: "); Serial.println(getDate());
   
-  scale.begin(16,17);   // 17 - portocaliu , 16 - galben
+  scale.begin(scalePin0,scalePin1);   
   scale.set_scale();
   scale.tare();  //Reset the scale to 0
   long zero_factor = scale.read_average(); //Get a baseline reading
   Serial.print("Zero factor: "); 
   Serial.println(zero_factor);
 
-    // Now set up two tasks to run independently.
+    //////Create aditional Tasks for Core 0//////
     xTaskCreatePinnedToCore(
-    TaskElastic
-    ,  "Elastic"   // A name 
-    ,  5120  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+        TaskElastic, /* Task function.                            */
+        "Task1",     /* name of task.                             */
+        1024 * 7,    /* Stack size of task                        */
+        NULL,        /* parameter of the task                     */
+        1,           /* priority of the task                      */
+        NULL,        /* Task handle to keep track of created task */
+        0            /* Core                                      */
+    );
 
-  xTaskCreatePinnedToCore(
-    TaskReadScale
-    ,  "ReadScale"
-    ,  1024  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(
+        TaskReadScale, /* Task function.                            */
+        "Task2",       /* name of task.                             */
+        1024 * 7,      /* Stack size of task                        */
+        NULL,          /* parameter of the task                     */
+        1,             /* priority of the task                      */
+        NULL,          /* Task handle to keep track of created task */
+        0              /* Core                                      */
+    );  
+
+    xTaskCreatePinnedToCore(
+        TaskReadWaterLevel, /* Task function.                            */
+        "Task3",            /* name of task.                             */
+        1024 * 7,           /* Stack size of task                        */
+        NULL,               /* parameter of the task                     */
+        1,                  /* priority of the task                      */
+        NULL,               /* Task handle to keep track of created task */
+        0                   /* Core                                      */
+    );
 }
-
-
 
 void loop()
 {
@@ -104,7 +115,7 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskElastic(void *pvParameters)  // This is a task.
+void TaskElastic(void *pvParameters)  // This is a task for CLOUD processing data
 {
   (void) pvParameters;
   for (;;) // A Task shall never return or exit.
@@ -113,8 +124,11 @@ void TaskElastic(void *pvParameters)  // This is a task.
     String request = getRequest();  
     JsonArray response = parseResponse(request);
     handleEvents(response);
+
   }
 }
+
+//////Function that make a GET request to server//////
 String getRequest()
 {
   String getResponse = "empty";
@@ -129,7 +143,7 @@ String getRequest()
   }
   String httpGet = String("GET ") + urlGet + " HTTP/1.1\r\n" +
                        "Host: " + host + "\r\n" +
-                        "User-Agent: esp8266pet\r\n" +
+                        "User-Agent: esp32pet\r\n" +
                         "Authorization: Basic " + "ZWxhc3RpYzpBV2J0bUdkYTJRN0JJMmJZcGRqeUY0cWQ=" + "\r\n" +
                         "Connection: close\r\n" +
                         "Content-Type: application/json\r\n" + 
@@ -149,7 +163,7 @@ String getRequest()
   return getResponse;
   }
 
-////////
+//////Function that parse the response from the server//////
 JsonArray parseResponse(String response){
   Serial.println("parseResponse: response = " + response);
   DynamicJsonDocument doc(2048);
@@ -164,7 +178,8 @@ JsonArray parseResponse(String response){
   hitsArray = doc["hits"]["hits"];
   return hitsArray;
 }
-////////
+
+//////Function that handle all the events available//////
 void handleEvents(JsonArray events){
   Serial.printf("handleEvents events size: %d\n", events.size());
   for (JsonVariant oneEvent : events) {
@@ -173,19 +188,21 @@ void handleEvents(JsonArray events){
           setConfiguration(oneEvent);
           //event = "start";
           //controlFood(selectedFood);
-          controlWater();
+          //controlWater();
           const char* Once = "Once";
           const char* repeat = oneEvent["_source"]["repeat"].as<const char*>();
-          delay(200);
+          vTaskDelay(200);
           if(strcmp (repeat, Once) == 0){
             postDelete(oneEvent["_id"].as<const char*>());
           }
-        //  postDone();
+          vTaskDelay(200);
+          postDone();
           break;
         }
     }
 }
-////////
+
+//////Function that check if the event is ready to execute//////
 bool isCurrentTime(JsonVariant event) {
   time_t nowTime = now() + 2 * 60 * 60;
   time_t eventTime = getTime(event);
@@ -198,7 +215,8 @@ bool isCurrentTime(JsonVariant event) {
 
   return (getHour(nowTime) == getHour(eventTime) && ( (getMinute(eventTime) >= getMinute(nowTime) - 2) && (getMinute(eventTime) <= getMinute(nowTime) ) ) );
 }
-////////
+
+//////Functios that process time//////
 int getHour(time_t timest){
     struct tm *tmp = gmtime(&timest);
     int h = (timest / 3600) % 24;  
@@ -223,10 +241,10 @@ time_t getTime(JsonVariant event) {
   tm.Hour = h;
   tm.Minute = m;
   tm.Second = s;
-
   return makeTime(tm);
 }
-////////
+
+//////Function that set the configuration for the sensors//////
 void setConfiguration(JsonVariant event) {
   int setFoodWeight = event["_source"]["setFoodWeight"].as<int>();
   selectedFood = setFoodWeight;
@@ -244,6 +262,7 @@ void setConfiguration(JsonVariant event) {
   Serial.printf("setConfiguration repeat: %s\n", repeat);
 }
 
+//////Function that delete the executed event from the server//////
 void postDelete(const char* id){
   String docDelete = String("{\"query\":") + "{" + "\"match\":" + "{" + "\"_id\":\"" + id + "\"" + "}" + "}" + "}";
   WiFiClientSecure client;
@@ -257,7 +276,7 @@ void postDelete(const char* id){
   }
   String httpDelete = String("POST ") + urlDelete + " HTTP/1.1\r\n" +
                     "Host: " + host + "\r\n" +
-                    "User-Agent: esp8266pet\r\n" +
+                    "User-Agent: esp32pet\r\n" +
                     "Authorization: Basic " + "ZWxhc3RpYzpBV2J0bUdkYTJRN0JJMmJZcGRqeUY0cWQ=" + "\r\n" +
                     "Connection: close\r\n" +
                     "Content-Type: application/json\r\n" +
@@ -268,31 +287,42 @@ void postDelete(const char* id){
   Serial.println(line);
 }
 
-void TaskReadScale(void *pvParameters)  // This is a task.
+//////Function that makes a POST request with measured data//////
+void postDone()
 {
-  (void) pvParameters;
-
-  while (1)
+  String docDone = "{\"timestamp\":\"" + getDate() + "\"," +
+                   "\"event\":\"" + controlFood_e + "\"," +
+                   "\"food_weight[g]\":" + food_weight + "," +
+                   "\"water_level\":\"" + translateWaterLevel(waterLevel) + "\"" + "}";
+  WiFiClientSecure client;
+  Serial.print("Connecting to Cloud at: ");
+  Serial.println(host);
+  client.setInsecure();
+  int y = client.connect(host, httpsPort);
+  if (!y)
   {
-   /* if(controlFood_e == "start"){
-      controlFood();
-    }else if(controlWater_e == "start"){
-      controlWater();
-      }*/
+    Serial.println("connection failed: " + y);
   }
+  String httpDone = String("POST ") + urlPost + " HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n" +
+                    "User-Agent: esp32pet\r\n" +
+                    "Authorization: Basic " + "ZWxhc3RpYzpBV2J0bUdkYTJRN0JJMmJZcGRqeUY0cWQ=" + "\r\n" +
+                    "Connection: close\r\n" +
+                    "Content-Type: application/json\r\n" +
+                    "Content-Length: " + docDone.length() + "\r\n\r\n" + docDone;
+  client.print(httpDone);
+  String line = client.readStringUntil('\n');
+  Serial.println("Reply was:");
+  Serial.println(line);
 }
 
-void controlWater()
-{
-  
-}
-
+//////Function that return the date in the timestamp format//////
 String getDate()
 {
-  String date = String(year()) + '-' + printDigits(month()) + '-' + printDigits(day()) + 'T' + printDigits(hour() + 3) + ':' + printDigits(minute()) + ':' + printDigits(second()) + 'Z';
+  String date = String(year()) + '-' + printDigits(month()) + '-' + printDigits(day()) + 'T' + printDigits(hour() + 2) + ':' + printDigits(minute()) + ':' + printDigits(second()) + 'Z';
   return date;
 }
-
+//////
 String printDigits(int digits)
 {
   String dig;
@@ -301,4 +331,112 @@ String printDigits(int digits)
     dig = "0";
   dig = dig + String(digits);
   return dig;
+}
+
+void TaskReadWaterLevel(void *pvParameters)  // This is the second task made for water level sensor 
+{
+  (void) pvParameters;
+
+  while (1)
+  {
+    vTaskDelay(1000);
+    Serial.println("Task TaskReadWaterLevel");
+    if(controlWater_e == "start"){
+      controlWater();
+      }
+  }
+}
+//////Function that control the water level using pump and the water level sensor//////
+void controlWater()
+{
+  if (selectedWater == 0)
+    selectedWater = 1;
+  measureWater();
+  while (waterLevel < selectedWater)
+  {
+    digitalWrite(relayPin, HIGH);  // open pump
+    measureWater();
+    vTaskDelay(50);
+  }
+  digitalWrite(relayPin, LOW); // close pump
+}
+void measureWater()
+{
+  int value = 0;
+  //Take a 50 reads average value
+  for (int i = 0; i < 50; i++)
+  {
+    value = value + analogRead(waterSensorPin);
+  }
+  int averageRead = value / 50;
+  Serial.print("Water level sensor measured value: ");
+  Serial.print(averageRead);Serial.println();
+  if (averageRead <= 88)
+  {
+    waterLevel = 0;
+  }
+  if (averageRead > 88 && averageRead <= 190)
+  {
+    waterLevel = 1;
+  }
+  if (averageRead > 190 && averageRead <= 240)
+  {
+    waterLevel = 2;
+  }
+  if (averageRead > 240)
+  {
+    waterLevel = 3;
+  }
+  Serial.print("Current water level(0..3): ");
+  Serial.print(waterLevel);Serial.println();
+}
+
+String translateWaterLevel(int level){
+  String water_level = "";
+  if(level == 0){
+   water_level = "Empty";
+  }else if(level == 1){
+    water_level = "Low";
+      }else if(level == 2){
+      water_level = "Medium";
+        }else if(level == 3){
+        water_level = "High";
+          }
+          return water_level;
+}
+
+void TaskReadScale(void *pvParameters)  //This is the third task made for scale
+{
+  (void) pvParameters;
+
+  while (1)
+  {
+    vTaskDelay(1000);
+    Serial.println("Task TaskReadScale");
+    if(controlFood_e == "start"){
+      controlFood(selectedFood);
+      }
+  }
+}
+//////Function used to control the food using the scale//////
+void controlFood(int selectedWeight)
+{
+  float units;
+  while (controlFood_e == "start")
+  {
+    scale.set_scale(calibration_factor);  //Adjust scale to calibration factor value
+    // Calculate the new value in grams
+    Serial.print("Reading: ");
+    units = scale.get_units();
+    if (units < 0){
+      units = 0.00;
+    }
+    if (units >= selectedWeight){
+      food_weight = int(units);
+      controlFood_e = "done";
+      vTaskDelay(100);
+    }
+    Serial.print(units);
+    Serial.print(" grams");Serial.println();
+  }
 }
